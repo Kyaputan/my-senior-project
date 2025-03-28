e65b91f9b1e0a81a87f1c7f0240fd77e0522d11d
Rachata Singkhet <singkhet1@gmail.com>
2025-03-28 20:29:59 +0700

    Update Test_Yolo.py

--- Tests/Test_Yolo.py
+++ Tests/Test_Yolo.py
@@ -122,8 +122,9 @@ if __name__ == "__main__":
     logging.info("Starting YOLO detection system")
     video_path = os.path.join(folder_path, "Videos", "Video1.mp4")
     logging.info(f"Using video source: {video_path}")
-    cap = cv2.VideoCapture(video_path)
+    # cap = cv2.VideoCapture(video_path)
     # cap = cv2.VideoCapture("rtsp://Rachata:12461246@192.168.0.101:554/stream1")
+    cap = cv2.VideoCapture("http://172.16.11.255:8080/video")
     now = time.time()
     format_time = datetime.fromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S')
     logging.info(f"Start at : {format_time}")
@@ -133,7 +134,7 @@ if __name__ == "__main__":
             logging.warning("Failed to read frame from video")
             break  
         
-        for _ in range(10):
+        for _ in range(5):
             cap.grab()
             
         output_frame = detect_yolo(frame)  
